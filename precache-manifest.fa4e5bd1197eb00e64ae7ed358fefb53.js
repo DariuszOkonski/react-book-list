642876e6ec4526b7c497dc37bfa9cf4332c409c6
Dariusz Okonski <darek200180@gmail.com>
2020-03-03 08:51:40 +0100

    Updates

---
+++ precache-manifest.fa4e5bd1197eb00e64ae7ed358fefb53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b5a8aa12d08a96537f9dc426edb365",
+    "url": "/react-book-list/index.html"
+  },
+  {
+    "revision": "14991e9e8ecde2b5d43a",
+    "url": "/react-book-list/static/css/main.a5a0f3cc.chunk.css"
+  },
+  {
+    "revision": "0e9b3845baf5119360bf",
+    "url": "/react-book-list/static/js/2.6b49be3e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-book-list/static/js/2.6b49be3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14991e9e8ecde2b5d43a",
+    "url": "/react-book-list/static/js/main.ade10d02.chunk.js"
+  },
+  {
+    "revision": "cc3416534d1a4f6e06e4",
+    "url": "/react-book-list/static/js/runtime-main.bc28cea7.js"
+  }
+]);
