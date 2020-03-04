3e325adfbb4ced77c8e8fd06a3ab2c2f69a311c0
Dariusz Okonski <darek200180@gmail.com>
2020-03-04 08:46:02 +0100

    change: tag div to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div id="header">
+    <header id="header">
       <h2 className="head">Header</h2>
       <p className="paragraph">Currently you have 2 books to get through</p>
-    </div>
+    </header>
   );
 }
 
