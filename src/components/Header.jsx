a6b98dc8892f4c2dca05e606b5e74c8b01d3b405
Dariusz Okonski <darek200180@gmail.com>
2020-03-04 07:58:56 +0100

    add: Header component with _header.scss

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div id="header">
+      <h2 className="head">Header</h2>
+      <p className="paragraph">Currently you have 2 books to get through</p>
+    </div>
+  );
+}
+
+export default Header;
