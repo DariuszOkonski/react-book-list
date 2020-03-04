31b8c5cf971003222d3d783bf186a2ec4658912f
Dariusz Okonski <darek200180@gmail.com>
2020-03-04 09:05:49 +0100

    add: BookList and remove Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import BookList from './views/BookList';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <main className="app">
         <Navigation />
-
-        <Header />
+        <BookList />
       </main>
     </BrowserRouter>
   );
