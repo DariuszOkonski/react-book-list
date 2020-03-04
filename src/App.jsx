a67e8f57d5d99676b5509f98059c60ffa473ddd6
Dariusz Okonski <darek200180@gmail.com>
2020-03-04 08:44:12 +0100

    wrap: hole app with BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <header className="app">
+        <Navigation />
+
+        <Header />
+      </header>
+    </BrowserRouter>
   );
 }
 
