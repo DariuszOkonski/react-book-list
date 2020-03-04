1c3244a76c1145816ed2cd8a8aa975b6ea3d26ab
Dariusz Okonski <darek200180@gmail.com>
2020-03-04 08:51:55 +0100

    add: defaultProps, propTypes and props title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <header id="header">
-      <h2 className="head">Header</h2>
+      <h2 className="head">{title}</h2>
       <p className="paragraph">Currently you have 2 books to get through</p>
     </header>
   );
+}
+
+Header.defaultProps = {
+  title: 'Book List'
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 }
 
 export default Header;
