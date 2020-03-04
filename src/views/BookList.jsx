3730cae4167494c8a2ad15c40cbfdd4628eb05cf
Dariusz Okonski <darek200180@gmail.com>
2020-03-04 09:04:27 +0100

    add: BookList view

---
+++ src/views/BookList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const BookList = () => {
+  return (
+    <div className="view">
+      <Header title="Book List" />
+    </div>
+  );
+}
+
+export default BookList;
