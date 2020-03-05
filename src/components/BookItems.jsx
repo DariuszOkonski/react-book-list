810fa79d4d5a8decfa954ae5f7be029d92fe0b78
Dariusz Okonski <darek200180@gmail.com>
2020-03-05 07:45:54 +0100

    add: BookItems component

---
+++ src/components/BookItems.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import BookItem from './BookItem';
+
+const BookItems = () => {
+  return (
+    <ul id="book-itmes">
+      <BookItem />
+      <BookItem />
+      <BookItem />
+      <BookItem />
+    </ul>
+  );
+}
+
+export default BookItems;
