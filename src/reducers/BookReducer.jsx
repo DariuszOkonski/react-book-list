e024eba40e93f306a8869328f6a7eb2e3e5a1268
Dariusz Okonski <darek200180@gmail.com>
2020-03-05 18:24:55 +0100

    add: ADD_BOOK to reducer

--- src/reducers/BookReducer.jsx
+++ src/reducers/BookReducer.jsx
@@ -1,5 +1,10 @@
+import { ADD_BOOK } from '../types/types';
+import uuid from 'uuid/v1';
+
 const bookReducer = (state, action) => {
   switch (action.type) {
+    case ADD_BOOK:
+      return [...state, { id: uuid(), title: action.book.title, author: action.book.author }];
     default:
       return state;
   }
