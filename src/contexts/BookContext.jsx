04ca30ecfe4e24f3242bcd83036691b2a83ce819
Dariusz Okonski <darek200180@gmail.com>
2020-03-05 09:03:56 +0100

    add: BookContextProvider

---
+++ src/contexts/BookContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useReducer } from 'react';
+import bookReducer from '../reducers/BookReducer';
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+  const [books, dispatch] = useReducer(bookReducer, [
+    { id: 1, author: 'Mario Puzo', title: 'Sicilian' },
+    { id: 2, author: 'Stieg Larsen', title: 'Girl with the dragon tattoo' },
+    { id: 3, author: 'Marion Puzo', title: 'The Goodfather' },
+    { id: 4, author: 'Dan Brown', title: 'The DaVinci Code' },
+  ])
+
+  return (
+    <BookContext.Provider value={{ books, dispatch }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+}
+
+export default BookContextProvider;
