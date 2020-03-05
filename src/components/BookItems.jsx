f37d6691666d69f2467a4731e24cec204f9deea4
Dariusz Okonski <darek200180@gmail.com>
2020-03-05 09:07:56 +0100

    add: BookContext and map after books recive from BookContext

--- src/components/BookItems.jsx
+++ src/components/BookItems.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import BookItem from './BookItem';
+import Empty from '../components/Empty';
+import { BookContext } from '../contexts/BookContext';
 
 const BookItems = () => {
-  return (
+  const { books } = useContext(BookContext);
+
+  return books.length > 0 ? (
     <ul id="book-itmes">
-      <BookItem />
-      <BookItem />
-      <BookItem />
-      <BookItem />
+      {
+        books.map(book => <BookItem key={book.id} {...book} />)
+      }
     </ul>
-  );
+  ) : (
+      <Empty information="There are not any books to read..." />
+    );
 }
 
 export default BookItems;
