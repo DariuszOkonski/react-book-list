9029cdcbe267fada75a4c53393d8d646c1ab0bb3
Dariusz Okonski <darek200180@gmail.com>
2020-03-06 08:50:51 +0100

    add: logic to DELETE_BOOK action

--- src/reducers/BookReducer.jsx
+++ src/reducers/BookReducer.jsx
@@ -1,10 +1,12 @@
-import { ADD_BOOK } from '../types/types';
+import { ADD_BOOK, DELETE_BOOK } from '../types/types';
 import { v1 } from 'uuid'
 
 const bookReducer = (state, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, { id: v1(), title: action.book.title, author: action.book.author }];
+    case DELETE_BOOK:
+      return state.filter(book => book.id !== action.id);
     default:
       return state;
   }
