034163505769b44f57812c180aac4b02287330ed
Dariusz Okonski <darek200180@gmail.com>
2020-03-06 10:06:58 +0100

    add: EDIT_BOOK logic

--- src/reducers/BookReducer.jsx
+++ src/reducers/BookReducer.jsx
@@ -1,4 +1,4 @@
-import { ADD_BOOK, DELETE_BOOK } from '../types/types';
+import { ADD_BOOK, DELETE_BOOK, EDIT_BOOK } from '../types/types';
 import { v1 } from 'uuid'
 
 const bookReducer = (state, action) => {
@@ -7,6 +7,13 @@ const bookReducer = (state, action) => {
       return [...state, { id: v1(), title: action.book.title, author: action.book.author }];
     case DELETE_BOOK:
       return state.filter(book => book.id !== action.id);
+    case EDIT_BOOK:
+      return state.map(book => {
+        if (book.id === action.book.id)
+          return { id: book.id, title: action.book.title, author: action.book.author }
+        else
+          return book;
+      });
     default:
       return state;
   }
