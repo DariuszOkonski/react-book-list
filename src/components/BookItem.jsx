75e9bb8f51ece7449d6475182c70d4a58e375d77
Dariusz Okonski <darek200180@gmail.com>
2020-03-06 08:49:17 +0100

    add: useHistory and method handleDelete to BookItem

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 
 const BookItem = ({ id, title, author }) => {
+  const history = useHistory()
+
+  const handleDelete = () => {
+    history.push('/remove', { deleteId: id });
+  }
+
   return (
     <li id="book-item">
       <section className="left">
-        <p><strong>{author}</strong></p>
-        <p>{title}</p>
+        <p className="paragraph"><strong>{title}</strong></p>
+        <p className="paragraph">{author}</p>
       </section>
 
       <section className="right">
-        <button className="btn">Delete</button>
+        <button onClick={handleDelete} className="btn">Delete</button>
         <button className="btn">Edit</button>
       </section>
     </li>
