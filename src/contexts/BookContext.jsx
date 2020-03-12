fa26b7b2ee7ba6a10536818e0771e4f275dc5d5c
Dariusz Okonski <darek200180@gmail.com>
2020-03-12 07:29:22 +0100

    remove: all unneccessary items

--- src/contexts/BookContext.jsx
+++ src/contexts/BookContext.jsx
@@ -6,8 +6,6 @@ export const BookContext = createContext();
 const BookContextProvider = (props) => {
   const [books, dispatch] = useReducer(bookReducer, [], () => {
     const localData = localStorage.getItem('books');
-    console.log(localData);
-
     return localData ? JSON.parse(localData) : [];
   });
 
@@ -23,8 +21,3 @@ const BookContextProvider = (props) => {
 }
 
 export default BookContextProvider;
-
-// { id: 1, author: 'Mario Puzo', title: 'Sicilian' },
-//     { id: 2, author: 'Stieg Larsen', title: 'Girl with the dragon tattoo' },
-//     { id: 3, author: 'Marion Puzo', title: 'The Goodfather' },
-//     { id: 4, author: 'Dan Brown', title: 'The DaVinci Code' },
