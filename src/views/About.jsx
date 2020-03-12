f165b9f091f58e18bf81fbd9b74039410839985a
Dariusz Okonski <darek200180@gmail.com>
2020-03-12 07:10:30 +0100

    add: About view

---
+++ src/views/About.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Header from '../components/Header';
+import AppInfo from '../components/AppInfo';
+
+const About = () => {
+  return (
+    <Fragment>
+      <Header title="About" />
+
+      <AppInfo />
+    </Fragment>
+  );
+}
+
+export default About;
