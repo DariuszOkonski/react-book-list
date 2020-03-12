00c13941f7306526635fbff409f453dc5dce5b93
Dariusz Okonski <darek200180@gmail.com>
2020-03-12 07:09:01 +0100

    add: AppInfo component

---
+++ src/components/AppInfo.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const AppInfo = () => {
+  return (
+    <div id="about">
+      <p className="paragraph"><strong>App Name: </strong>Book List</p>
+      <p className="paragraph"><strong>Author: </strong>Dariusz Okonski</p>
+      <p className="paragraph"><strong>Version: </strong>1.0.0</p>
+      <p className="paragraph"><strong>Year: </strong>2020</p>
+    </div>
+  );
+}
+
+export default AppInfo;
