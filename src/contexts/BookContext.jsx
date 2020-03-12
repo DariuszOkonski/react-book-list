4b669248ecd91fb93b576c4de3cf959b187b3b27
Dariusz Okonski <darek200180@gmail.com>
2020-03-12 07:22:40 +0100

    add: set and get data from localStorage

--- src/contexts/BookContext.jsx
+++ src/contexts/BookContext.jsx
@@ -1,15 +1,19 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import bookReducer from '../reducers/BookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, [
-    { id: 1, author: 'Mario Puzo', title: 'Sicilian' },
-    { id: 2, author: 'Stieg Larsen', title: 'Girl with the dragon tattoo' },
-    { id: 3, author: 'Marion Puzo', title: 'The Goodfather' },
-    { id: 4, author: 'Dan Brown', title: 'The DaVinci Code' },
-  ]);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books');
+    console.log(localData);
+
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books));
+  }, [books])
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
@@ -19,3 +23,8 @@ const BookContextProvider = (props) => {
 }
 
 export default BookContextProvider;
+
+// { id: 1, author: 'Mario Puzo', title: 'Sicilian' },
+//     { id: 2, author: 'Stieg Larsen', title: 'Girl with the dragon tattoo' },
+//     { id: 3, author: 'Marion Puzo', title: 'The Goodfather' },
+//     { id: 4, author: 'Dan Brown', title: 'The DaVinci Code' },
